5282f7674a269922e10168d57f74aaaac5733b78
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-07-22 11:20:24 -0400

    completed Section 10

--- app/models.py
+++ app/models.py
@@ -27,8 +27,9 @@ class Location(models.Model):
     return f"{self.state} - {self.street}"
 
 class JobPost(models.Model):
+  JOB_TYPE_CHOICES=[('Full Time', 'Full Time'),('Part Time','Part Time')]
   title = models.CharField(max_length=200)
-  description = models.TextField(max_length=5000)
+  description = models.TextField()
   post_date = models.DateTimeField(auto_now_add=True)
   salary = models.IntegerField()
   company = models.CharField(max_length=200)
@@ -38,6 +39,7 @@ class JobPost(models.Model):
   slug = models.SlugField(null=True, max_length=40, unique=True)
   author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
   skills = models.ManyToManyField(Skill)
+  type=models.CharField(max_length=200, null=False)
   
   def __str__(self):
     return f"{self.company} - {self.title}"
