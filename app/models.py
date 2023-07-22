fb9d9aef78dcada39624a46cc53e663f4e2c66a8
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-07-22 19:24:08 -0400

    completed migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title: models.CharField(max_length=200)
+  content = models.TextField()
+  last_updated = models.DateField()
+  slug = models.SlugField(max_length=200,unique=True)
+  image = models.ImageField(null=True, blank=True, upload_to='images/')
+  
