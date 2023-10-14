a452930f77293e59587046aeef322c5d6f469ab1
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 18:28:13 -0400

    bugs2

---
+++ bugs2.py
@@ -0,0 +1,19 @@
+# Bug-Fixing Exercise 1
+# The code below has two bugs. Hunt them down and fix them.
+#
+# while True
+# print("Hello")
+#
+while True:
+    print("Hello")
+
+
+# Bug-Fixing Exercise 2
+# The programmer here is trying to convert the string "hello" to "HELLO" by using the upper() method:
+#
+# greeting = "hello"
+# print(upper(greeting))
+# However, the program returns an error. Can you help fix the code, so it prints out HELLO?
+
+greeting = "hello"
+print(greeting.upper())
