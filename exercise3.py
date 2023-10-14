bd87628713add8547385bb802fef7496b8d6f972
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 18:27:55 -0400

    exercise3

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+# Create a program that prompts the user to input their name repeatedly. Then, the program repeatedly prints out the
+# name with the first letter capitalized.
+
+while True:
+    name = input("What is your name?")
+    print(name.capitalize())
+    
