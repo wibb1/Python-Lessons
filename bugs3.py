2f1533ec02e45d4746be724d33bb663e86d97c70
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 19:20:13 -0400

    bugs 3

---
+++ bugs3.py
@@ -0,0 +1,33 @@
+# The programmer is trying to loop over the buttons list and print out each item with the first letter
+# capitalized.However, the programmer has done something wrong.Try to find and fix the issue.
+
+# for i in buttons:
+#     print(i.capitalize())
+#
+# buttons = ["cancel", "reply", "submit"]
+
+buttons = ["cancel", "reply", "submit"]
+
+for i in buttons:
+    print(i.capitalize())
+
+# The programmer is again missing something in the code.Try to find what it is and fix it.
+
+# buttons = ["cancel", "reply", "submit"]
+#
+# for i in buttons:
+# print(i.capitalize())
+
+
+buttons = ["cancel", "reply", "submit"]
+
+for i in buttons:
+    print(i.capitalize())
+
+# The code below is supposed to print out the items of the list with the first character of each item capitalized. However, the code contains two errors. Try to find and fix the errors.
+#
+# for item in ["sandals", "glasses", "trousers"):
+#     print(item.capitalize)
+
+for item in ["sandals", "glasses", "trousers"]:
+    print(item.capitalize)
