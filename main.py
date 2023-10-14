af8cbd3fd19c5d5cccbcd1d7ff3a720f13f85fcd
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 18:53:33 -0400

    Adding strip to input

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-user_prompt1 = "Type add or show: "
+user_prompt1 = "Type add, show, or exit: "
 user_prompt2 = "Enter a Todo: "
 todos = []
 while True:
-    user_action = input(user_prompt1)
+    user_action = input(user_prompt1).strip()
     match user_action.lower():
         case 'add':
             todo = input(user_prompt2)
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
-            break
-        case 'stop':
-            break
-        case 'end':
             break
 print("Bye")
