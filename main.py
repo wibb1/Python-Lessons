a7ab77895c5064efe9067f9c53d525a0aa926e25
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 17:32:42 -0400

    First main

---
+++ main.py
@@ -0,0 +1,8 @@
+user_prompt1 = "Enter a todo:"
+todo1 = input(user_prompt1)
+todo2 = input(user_prompt1)
+todo3 = input(user_prompt1)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(todo1))
