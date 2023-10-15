62c87baa715497a6f27e8210f02f7eebc5c69089
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-14 21:00:36 -0400

    My take on edge cases

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-user_prompt1 = "Type add, show/display, or exit/stop: "
+user_prompt1 = "Type add, show/display, edit or exit/stop: "
 user_prompt2 = "Enter a Todo: "
-todos = []
+todos = ['first', 'second', 'third']
 while True:
     user_action = input(user_prompt1).strip()
     match user_action.lower():
@@ -9,9 +9,30 @@ while True:
             todos.append(todo)
         case 'show' | 'display':
             for item in todos:
-                print(item)
+                print(item.title())
         case 'exit' | 'stop':
             break
+        case 'edit':
+            valid = False
+            todo_count = len(todos)
+            while not valid:
+                if todo_count < 1:
+                    print("Nothing in your todos")
+                    break
+                try:
+                    index = input("Enter the number from the list you want to edit or quit: ")
+                    if index.strip() == 'quit':
+                        break
+                    index = int(index) - 1
+                    if -1 < index < todo_count:
+                        prompt = f"Enter the new value for {todos[index]}: "
+                        new_todo = input(prompt)
+                        todos[index] = new_todo
+                        valid = True
+                    else:
+                        print("Enter a value between one and", todo_count, "or quit")
+                except ValueError:
+                    print("Enter an integer")
         case _:
             print("You have entered an unknown command")
 print("Bye")
