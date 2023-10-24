09f2edbb7d19f0fe62b3c9e2273ae7021481d288
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-24 17:54:36 -0400

    start of gui app

---
+++ gui.py
@@ -0,0 +1,12 @@
+import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a todo")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My todo app", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
+
+
