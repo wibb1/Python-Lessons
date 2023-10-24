aa9d3dfe69d18b5778d595b0ab9a13216473679b
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-24 18:17:43 -0400

    bonus16

---
+++ bonus/bonus16.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text("Select files to compress")
+input1=sg.Input()
+choose_button1 = sg.FileBrowse("Choose")
+
+label2 = sg.Text("Select destination folder")
+input2=sg.Input()
+choose_button2 = sg.FileBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window("Zip tool",
+                   layout=[
+                       [label1, input1, choose_button1],
+                       [label2, input2, choose_button2],
+                       [compress_button]
+                   ])
+
+window.read()
+window.close()
