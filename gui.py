cb263cd19d5eabe486dc14563a7deb16ccea0002
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-10-28 12:03:15 -0400

    adding images to all buttons

--- gui.py
+++ gui.py
@@ -4,23 +4,27 @@ import functions
 import PySimpleGUI as sg
 import time
 
-sg.theme("DarkPurple4")
+sg.theme("Black")
 clock = sg.Text("", key='clock')
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.read_todos(), key='todos',
                       enable_events=True, size=(45, 10))
-edit_button = sg.Button('Edit')
-done_button = sg.Button('Done')
-exit_button = sg.Button('Exit')
+add_button  = sg.Button(size=5, image_source='add.png', mouseover_colors="LightBlue2", tooltip="Add todo", key='Add')
+
+edit_button = sg.Button(size=5, image_source='edit.png', mouseover_colors="LightBlue2", tooltip="Edit todo", key='Edit')
+
+done_button = sg.Button(size=5, image_source='complete.png', mouseover_colors="LightBlue2", tooltip="Remove todo", key='Done')
+
+exit_button = sg.Button(size=5, image_source='exit.png', mouseover_colors="LightBlue2", tooltip="Exit Program", key='Exit')
+
 window = sg.Window("My todo app",
                    layout=[[clock], [label], [input_box, add_button], [list_box, edit_button, done_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read(timeout=1000)
+    event, values = window.read(timeout=100)
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case 'Add':
