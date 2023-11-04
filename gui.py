b32302370dd2c97c53c13a324a2ef41f50221946
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-03 21:03:57 -0400

    gui executable

--- gui.py
+++ gui.py
@@ -3,6 +3,12 @@ from PySimpleGUI import Window
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 sg.theme("Black")
 clock = sg.Text("", key='clock')
