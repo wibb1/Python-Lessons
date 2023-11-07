efe4e686d78de6f93f41e1eb00d0df9b34e1005c
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-07 18:15:09 -0500

    added description, images, and links

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
+col1, empty_column, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image('images/photo.png')
@@ -21,10 +21,16 @@ col3, col4 = st.columns(2)
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({'url'})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({'url'})")
 
 
 
