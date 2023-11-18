c2476b38e5c2e2e6c9a5a92d46d30bbcd7e60ea7
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-18 16:41:12 -0500

    Loading data from spreadsheets

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import glob
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def get_files(file_location):
+    file_location = file_location + "*.xlsx"
+    filepaths = glob.glob(file_location)
+
+    for filepath in filepaths:
+        df = pd.read_excel(filepath, sheet_name="Sheet 1")
+        print(df)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_files("invoices/")
