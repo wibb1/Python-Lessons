a25d79d5b4689710473772b5ebbb317d6e731e55
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-24 14:04:24 -0500

    Beginning invoice format

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 
-
-def get_files(file_location):
+def get_filepaths(file_location):
     file_location = file_location + "*.xlsx"
-    filepaths = glob.glob(file_location)
+    return glob.glob(file_location)
 
-    for filepath in filepaths:
-        df = pd.read_excel(filepath, sheet_name="Sheet 1")
-        print(df)
+
+def build_header(pdf, filename):
+    pdf.add_page()
+    pdf.set_font(family='Times', size=16, style='B')
+    invoice_info = filename.split('-')
+    pdf.cell(w=50, h=8, txt="Invoice: " + invoice_info[0])
+    pdf.cell(w=50, h=8, txt="Date: " + invoice_info[1])
+    pdf.output(f"PDFs/{filename}.pdf")
 
 
 if __name__ == '__main__':
-    get_files("invoices/")
+    filepaths = get_filepaths("invoices/")
+
+    for filepath in filepaths:
+        df = pd.read_excel(filepath, sheet_name="Sheet 1")
+        filename = Path(filepath).stem
+        pdf = FPDF(orientation='P', unit="mm", format="A4")
+        build_header(pdf, filename)
