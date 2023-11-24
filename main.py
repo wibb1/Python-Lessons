7727c86b4ef69c6a72405a99269c8de46eee0124
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-24 17:36:18 -0500

    Add table

--- main.py
+++ main.py
@@ -12,17 +12,44 @@ def get_filepaths(file_location):
 def build_header(pdf, filename):
     pdf.add_page()
     pdf.set_font(family='Times', size=16, style='B')
-    invoice_info = filename.split('-')
-    pdf.cell(w=50, h=8, txt="Invoice: " + invoice_info[0])
-    pdf.cell(w=50, h=8, txt="Date: " + invoice_info[1])
-    pdf.output(f"PDFs/{filename}.pdf")
+    invoice_no, invoice_date = filename.split('-')
+    pdf.cell(w=50, h=8, txt="Invoice: " + invoice_no, ln=1)
+    pdf.cell(w=50, h=8, txt="Date: " + invoice_date, ln=1)
+
+
+def add_table_headers(headers, pdf):
+    headers = [sub.replace("_", " ").title() for sub in headers]
+    pdf.cell(w=30, h=8, txt=str(headers[0]), border=1)
+    pdf.cell(w=60, h=8, txt=str(headers[1]), border=1)
+    pdf.cell(w=40, h=8, txt=str(headers[2]), border=1)
+    pdf.cell(w=30, h=8, txt=str(headers[3]), border=1)
+    pdf.cell(w=30, h=8, txt=str(headers[4]), border=1, ln=1)
+    pdf.set_font(family='Times', size=10, style='')
+
+
+def add_table_data(rows, pdf):
+    for index, row in rows:
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+
+def build_table(pdf, df):
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    add_table_headers(df.columns, pdf)
+    add_table_data(df.iterrows(), pdf)
 
 
 if __name__ == '__main__':
     filepaths = get_filepaths("invoices/")
 
     for filepath in filepaths:
-        df = pd.read_excel(filepath, sheet_name="Sheet 1")
+        df = pd.read_excel(filepath, sheet_name="Sheet 1", header=0)
         filename = Path(filepath).stem
         pdf = FPDF(orientation='P', unit="mm", format="A4")
         build_header(pdf, filename)
+        build_table(pdf, df)
+        pdf.output(f"PDFs/{filename}.pdf")
