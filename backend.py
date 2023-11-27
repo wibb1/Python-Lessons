c4c2f29eb70280fb38fd6011059bfab81df4170c
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-27 18:53:00 -0500

    Changed backend to only get data from api

--- backend.py
+++ backend.py
@@ -2,21 +2,18 @@ from dotenv import dotenv_values
 import requests
 
 config = dotenv_values(".env")
+READINGS_PER_DAY = 8
 
 
-def get_data(place, days, view_data):
-    from dotenv import dotenv_values
-    config = dotenv_values(".env")
+def get_data(place, days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={config['API_KEY']}"
     response = requests.get(url)
-    content = response.json()
-    return content
-    # dates = ["2022-25-10", "2023-25-10", "2023-26-12"]
-    # temperatures = [25, 11, 10]
-    # temperatures = [days * i for i in temperatures]
-    # return dates, temperatures
+    data = response.json()
+    return data['list'][:READINGS_PER_DAY * days]
 
 
 if __name__ == '__main__':
-    data = get_data('Tokyo', 5, 'Temperature')
-    print(data)
+    data1 = get_data('Tokyo', 2)
+    print(data1)
+    data2 = get_data('Tokyo', 2)
+    print(data2)
