48b1f6584c8691f43ad72e1faf5e18440d72eb6f
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-11-27 19:49:29 -0500

    added units to api call

--- backend.py
+++ backend.py
@@ -5,8 +5,8 @@ config = dotenv_values(".env")
 READINGS_PER_DAY = 8
 
 
-def get_data(place, days):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={config['API_KEY']}"
+def get_data(place, days, units):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&units={units}&appid={config['API_KEY']}"
     response = requests.get(url)
     data = response.json()
     return data['list'][:READINGS_PER_DAY * days]
