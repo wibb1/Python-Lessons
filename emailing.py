1da387c2905c3c9161362123288e21dc3558a605
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-12-16 15:56:47 -0500

    Completing email setup

--- emailing.py
+++ emailing.py
@@ -1,1 +1,33 @@
+import imghdr
+import smtplib
+from email.message import EmailMessage
+from dotenv import dotenv_values
 
+
+def send_email(image_path):
+    host = "smtp.gmail.com"
+    port = 587
+    config = dotenv_values(".env")
+    username = config["USERNAME"]
+    password = config["PASSWORD"]
+    receiver = "willcampbell030@gmail.com"
+
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Sighting"
+    email_message.set_content(f"A new sighting occurred!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP(host, port)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(username, password)
+    gmail.sendmail(username, receiver, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == '__main__':
+    send_email("images/file_example_PNG_500kB.png")
