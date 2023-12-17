4b3abf0b273101ab5297d8cc634a872407fc607c
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-12-17 11:35:40 -0500

    Initial extraction

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape():
+    # Scrape the page source from the URL.
+    response = requests.get(URL, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    data = scrape()
+    extracted = extract(data)
+    print(extracted)
