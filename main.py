284f7e996390b596daf8e22fdcb02bbc8e1975d9
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-12-17 09:21:45 -0500

    Sending email with frame attached

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import glob
+import os
 import time
-
+import datetime
 import cv2
+
+from emailing import send_email
+
+
+def empty_images_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 if __name__ == '__main__':
     first_frame = None
@@ -8,8 +19,10 @@ if __name__ == '__main__':
     video = cv2.VideoCapture(0)
     time.sleep(1)
 
-    while True:
+    status_list = []
 
+    while True:
+        status = 0
         check, frame = video.read()
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray_frame_gua = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -17,9 +30,9 @@ if __name__ == '__main__':
         if first_frame is None:
             first_frame = gray_frame_gua
 
-        delta_frame = cv2.absdiff(gray_frame_gua, first_frame)
+        delta_frame = cv2.absdiff(first_frame, gray_frame_gua)
 
-        thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)
+        thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
         dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
         cv2.imshow("Video", dil_frame)
 
@@ -28,8 +41,27 @@ if __name__ == '__main__':
         for contour in contours:
             if cv2.contourArea(contour) < 5000:
                 continue
+            # Object detected
             x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
+            rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
+            if rectangle.any():
+                status = 1
+                date_time = datetime.datetime.now()
+                format_datetime = date_time.strftime("%Y%m%d%H%M%S.%f")
+                print(format_datetime)
+                cv2.imwrite(f"images/{format_datetime}.png", frame)
+                all_images = glob.glob("images/*.png")
+                print(str(len(all_images)))
+                index = int(len(all_images) / 2)
+                image_with_object = all_images[index]
+
+        status_list.append(status)
+        status_list = status_list[-2:]
+
+        if status_list[0] == 1 and status_list[1] == 0:
+            send_email(image_with_object)
+            empty_images_folder()
+
         cv2.imshow("Video", frame)
         key = cv2.waitKey(1)
 
