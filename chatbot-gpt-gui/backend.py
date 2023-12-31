bf2b413bfa642fa511c933f4f927df7a558a766b
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-12-31 15:26:53 -0500

    completed chatbot app

--- chatbot-gpt-gui/backend.py
+++ chatbot-gpt-gui/backend.py
@@ -1,2 +1,23 @@
+import openai
+from dotenv import dotenv_values
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        config = dotenv_values(".env")
+        openai.api_key = config['OPENAI_API_KEY']
+
+    def get_response(self, user_input):
+        completion = openai.completions.create(
+            model="gpt-3.5-turbo",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return completion
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("Write a dirty joke about fisherman")
+    print(response)
--- chatbot-gpt-gui/main.py
+++ chatbot-gpt-gui/main.py
@@ -2,11 +2,16 @@ import sys
 
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QPushButton, QLineEdit, QApplication
 
+from backend import Chatbot
+
+import threading
+
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setMinimumSize(700,500)
+        self.chatbot = Chatbot()
+        self.setMinimumSize(700, 500)
 
         self.chat_area = QTextEdit(self)
         self.chat_area.setGeometry(40, 40, 480, 320)
@@ -14,11 +19,24 @@ class ChatbotWindow(QMainWindow):
 
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(40, 400, 480, 48)
+        self.input_field.returnPressed.connect(self.send_message)
 
         self.button = QPushButton("Send", self)
         self.button.setGeometry(560, 400, 110, 48)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>User: {user_input}</p>")
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input,))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333'; backgroud-color:#e9e9e9>Chatbot: {response}</p>")
 
 
 def main():
